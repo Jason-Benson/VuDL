0ab902a60ea2572a4a1dbecc892c80bc2baaa702
Demian Katz <demian.katz@villanova.edu>
2026-01-08 09:58:26 -0500

    Simplify mocking.

--- api/jest.config.js
+++ api/jest.config.js
@@ -1,9 +1,5 @@
 module.exports = {
     preset: "ts-jest",
     testEnvironment: "node",
-    testPathIgnorePatterns: ["<rootDir>/dist/"],
-    // Map the `nanoid` import to a lightweight mock during tests to avoid ESM import parsing.
-    moduleNameMapper: {
-        "^nanoid$": "<rootDir>/__mocks__/nanoid.js",
-    },
+    testPathIgnorePatterns: ["<rootDir>/dist/"]
 };
